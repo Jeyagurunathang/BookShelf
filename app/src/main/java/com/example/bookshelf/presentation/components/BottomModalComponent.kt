37c77b658ead6278ede6a59ddfaa4d2ebeb4a643
Jeyagurunathan G <jeyagurunathang@gmail.com>
2025-05-10 15:58:57 +0530

    resolved the title issue & implemented bottom modal

---
+++ app/src/main/java/com/example/bookshelf/presentation/components/BottomModalComponent.kt
@@ -0,0 +1,67 @@
+package com.example.bookshelf.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.bookshelf.R
+import com.example.bookshelf.presentation.core.ui.theme.BookShelfTheme
+
+@Composable
+fun BottomModalComponent(
+    modifier: Modifier = Modifier,
+    bookDescription: String,
+    bookTitle: String
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_tiny))
+    ) {
+        Text(
+            text = bookTitle,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+
+        Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_tiny)))
+
+        Text(
+            text = bookDescription,
+            color = MaterialTheme.colorScheme.onPrimary,
+            style = MaterialTheme.typography.labelMedium,
+            maxLines = 7,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun BottomModalComponentPreview(modifier: Modifier = Modifier) {
+    BookShelfTheme(dynamicColor = false) {
+        BottomModalComponent(
+            bookDescription = "Android Development",
+            bookTitle = "Android Development"
+        )
+    }
+}
--- app/src/main/java/com/example/bookshelf/presentation/getBooks/ListScreenBody.kt
+++ app/src/main/java/com/example/bookshelf/presentation/getBooks/ListScreenBody.kt
@@ -1,10 +1,14 @@
 package com.example.bookshelf.presentation.getBooks
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import com.example.bookshelf.R
 import com.example.bookshelf.domain.models.booksListModel.Item
 import com.example.bookshelf.presentation.components.BookCardComponent
 
@@ -14,7 +18,10 @@ fun ListScreenBody(
     books: List<Item>
 ) {
     LazyVerticalGrid(
-        columns = GridCells.Fixed(2)
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(dimensionResource(R.dimen.padding_tiny)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_tiny)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_tiny))
     ) {
         items(
             items = books,
